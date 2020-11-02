0893bcd6ee6e2dfb60aebdf47007cf5c813cf310
MagHSK <maghsk2017@gmail.com>
2020-11-02 12:50:06 +0800

    First Commit
    Signed-off-by: MagHSK <maghsk2017@gmail.com>

---
+++ src/main/java/org/pkuse2020grp4/pkusporteventsbackend/PkusporteventsbackendApplication.java
@@ -0,0 +1,13 @@
+package org.pkuse2020grp4.pkusporteventsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkusporteventsbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkusporteventsbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pkuse2020grp4/pkusporteventsbackend/PkusporteventsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pkuse2020grp4.pkusporteventsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkusporteventsbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
