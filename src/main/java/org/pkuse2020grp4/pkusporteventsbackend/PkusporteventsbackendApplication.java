e216b2c177c5862e5eaddae018470a88347d7cb9
MagHSK <maghsk2017@gmail.com>
2020-11-03 14:37:17 +0800

    Envs: Updated maven dependencies
    Signed-off-by: MagHSK <maghsk2017@gmail.com>

--- src/main/java/org/pkuse2020grp4/pkusporteventsbackend/PkusporteventsbackendApplication.java
+++ src/main/java/org/pkuse2020grp4/pkusporteventsbackend/PkusporteventsbackendApplication.java
@@ -2,12 +2,15 @@ package org.pkuse2020grp4.pkusporteventsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class PkusporteventsbackendApplication {
+
+public class PkuSportEventsBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PkusporteventsbackendApplication.class, args);
+        SpringApplication.run(PkuSportEventsBackendApplication.class, args);
     }
-
 }
