c6c92df3b63d20f96d820c4bb0ae5acad16d5435
SpiritSoda <1800013119@pku.edu.cn>
2020-12-27 20:01:21 +0800

    [Bug Fixed] Refix shiro CORS request

--- src/main/java/org/pkuse2020grp4/pkusporteventsbackend/filter/URLPathMatchingFilter.java
+++ src/main/java/org/pkuse2020grp4/pkusporteventsbackend/filter/URLPathMatchingFilter.java
@@ -29,12 +29,11 @@ public class URLPathMatchingFilter extends PathMatchingFilter {
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception{
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", httpServletRequest.getMethod());
-        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
         // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
         if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+            httpServletResponse.setHeader("Access-Control-Allow-Methods", httpServletRequest.getMethod());
+            httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
             httpServletResponse.setStatus(HttpStatus.OK.value());
             return true;
         }
@@ -45,6 +44,13 @@ public class URLPathMatchingFilter extends PathMatchingFilter {
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception{
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+            httpServletResponse.setHeader("Access-Control-Allow-Methods", httpServletRequest.getMethod());
+            httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+            httpServletResponse.setStatus(HttpStatus.OK.value());
+            return true;
+        }
 
         String requestAPI = getPathWithinApplication(request);
 
